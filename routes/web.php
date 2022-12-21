e108d96f2cf4b886b410e2aaa8afcecf4b9ed0f2
luthfizh <luthfi.izzuddin1@gmail.com>
2022-12-21 13:34:18 +0700

    feat: slicing and integrate user dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\User\CheckoutController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,15 +24,6 @@ Route::get('login', function () {
     return view('login');
 })->name('login');
 
-Route::get('checkout/success', [CheckoutController::class, 'success'])
-    ->name('checkout.success');
-
-Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])
-    ->name('checkout.create');
-
-Route::post('checkout/{camp}', [CheckoutController::class, 'store'])
-    ->name('checkout.store');
-
 // socialite routes
 Route::get('sign-in-google', [UserController::class, 'google'])
     ->name('user.login.google');
@@ -39,8 +31,26 @@ Route::get('sign-in-google', [UserController::class, 'google'])
 Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])
     ->name('user.auth.callback');
 
-Route::get('dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+    // checkout routes
+    Route::get('checkout/success', [CheckoutController::class, 'success'])
+        ->name('checkout.success');
+
+    Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])
+        ->name('checkout.create');
+
+    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])
+        ->name('checkout.store');
+
+    // user dashboard
+    Route::get('dashboard', [HomeController::class, 'dashboard'])
+        ->name('dashboard');
+});
+
+
+
+// Route::get('dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
